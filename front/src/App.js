dfc93b70e708eb7576a4a132e6608f0eeb0ab62d
Isaac Njenga <111574568+IsaacNjenga@users.noreply.github.com>
2024-03-29 12:15:23 +0300

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -45,7 +45,7 @@ function App() {
               <td>{image._id}</td>
               <td>
                 <img
-                  src={`http://localhost:3001/Images/${image.image}`}
+                  src={`https://image-vercel-api.vercel.app/Images/${image.image}`}
                   style={{
                     width: "500px",
                     height: "330px",
